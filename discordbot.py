6d171291b505f9745e95fcaae596f3986fca6b74
syunnmaro <61906816+syunnmaro@users.noreply.github.com>
2020-10-23 00:46:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -17,5 +17,21 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+async def create_channel(message, channel_name):
+    category_id = message.channel.category_id
+    category = message.guild.get_channel(category_id)
+    new_channel = await category.create_text_channel(name=channel_name)
+    return new_channel
 
-bot.run(token)
+# 発言時に実行されるイベントハンドラを定義
+@client.event
+async def on_message(message):
+    if message.content.startswith('/mkch'):
+        # チャンネルを作成する非同期関数を実行して Channel オブジェクトを取得
+        new_channel = await create_channel(message, channel_name='new')
+
+        # チャンネルのリンクと作成メッセージを送信
+        text = f'{new_channel.mention} を作成しました'
+        await message.channel.send(text)
+
+        bot.run(token)
